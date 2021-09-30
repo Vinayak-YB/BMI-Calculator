e479080e83e5db6752192f12a1018370e0d3c07b
Vinayak Y B <vinayak.bhustali@gmail.com>
2021-09-30 19:11:34 +0530

    Build update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Body Mass Index Calculator 
+
+Body Mass Index (BMI) is a person’s weight in kilograms divided by the square of height in meters. BMI screens for weight categories that may lead to health problems, but it does not diagnose the body fatness or health of an individual. This is a JavaScript application to measure Body Mass Index 
+
+Link:
--- script.js
+++ script.js
@@ -10,8 +10,13 @@ function submitHandler()
     var b = Number(Weight.value)
     console.log(Height.value);
     console.log(Weight.value);
+    if(a>0 && b>0){
     var answer = b / (a*a)
     answer = answer.toFixed(2);
     console.log(answer);
     output.innerText = "Your BMI is " + answer;
+    }
+    else{
+        output.innerText="Enter positive values";
+    }
 };
